cfa23f25f11cab2a231eac48d67d34848a9e411c
mfcastilho <fredericocastilho@hotmail.com>
2024-09-12 03:35:52 -0300

    refactor: refactor the exercise descriptions

--- src/exercicio3.js
+++ src/exercicio3.js
@@ -1,4 +1,10 @@
-/*3) Observe o trecho de código abaixo: int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
+/*3) Observe o trecho de código abaixo: 
+
+int INDICE = 12, SOMA = 0, K = 1; 
+  enquanto K < INDICE faça { 
+    K = K + 1; SOMA = SOMA + K; 
+  } 
+imprimir(SOMA);
 
 Ao final do processamento, qual será o valor da variável SOMA? */
 
