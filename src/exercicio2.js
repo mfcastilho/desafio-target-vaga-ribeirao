8afa7b98f50b3ae426e2c02ee59a40fceccceb03
mfcastilho <fredericocastilho@hotmail.com>
2024-09-12 03:27:31 -0300

    update: insert the exercise description at the exercicio2 file

--- src/exercicio2.js
+++ src/exercicio2.js
@@ -1,3 +1,7 @@
+/*2) Escreva um programa que verifique, em uma string, a existência da letra ‘a’, seja maiúscula ou minúscula, além de informar a quantidade de vezes em que ela ocorre.
+
+IMPORTANTE: Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código; */
+
 const normalizeWord = (word) => {
   return word
     .normalize('NFD')
@@ -9,17 +13,20 @@ const findLetterA = (word) => {
   const wordNormalized = normalizeWord(word);
   let count = 0;
 
-  wordNormalized.split('').forEach((letter, index) => {
+  wordNormalized.split('').forEach(letter => {
     if (letter === 'a') {
       count++;
     }
   });
 
+  if (count === 0) {
+    return `A letra A não foi encontrada na palavra ${word}`;
+  }
 
   return `Letra A foi encontrada ${count} vez(es) na palavra '${word}'`;
 }
 
-console.log(findLetterA('LimãAaáo'));
+console.log(findLetterA('MAriâná'));
 
 
 module.exports = findLetterA;
